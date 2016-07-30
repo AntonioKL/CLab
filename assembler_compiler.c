b4fbb921eac8d8db183f1efcb3c7806eec1c109e
AntonioKL <student@ubuntu.(none)>
2016-07-30 14:02:07 -0400

    It is time for some comments

--- assembler_compiler.c
+++ assembler_compiler.c
@@ -5,7 +5,7 @@ Main File
 	Git Project: https://github.com/AntonioKL/CLab
 	Description: Simulation of an assembler with an output in unique 8 base encoding.
 	Input : File names to convert without .as extensions
-	Output :  1) Syntax Errors and Warnings
+	Output :  1) Syntax Errors and their location in the .as file
 		  2) For valid file we will create several files with the following extensions:
 			- .ob: encoded operation for linker
 			- .ext: memory address of external variable
@@ -20,8 +20,8 @@ Main File
 The main complier function that will run on provided files and parse them
 Input: Assembler files as command line arguments, without the .as postfix.
 Output: 
-	1) Syntax Errors and Warnings
-	2) For valid file we will create several files with the following 		   extensions:
+	1) Syntax Errors and their location in the .as file
+	2) For valid file we will create several files with the following extensions:
 		- .ob: encoded operation for linker
 		- .ext: memory address of external variable
 		- .ent: memory address of the labels
@@ -33,29 +33,25 @@ int main(int argc, char *argv[])
 	
 	if (argc < MIN_ARGS)
 	{
-		printf("ERROR: No file name, please provide the script with a file to run on, at least one file.\n");
+		printf("ERROR: No file name, please provide the script with a least one file to run on.\n");
 		return 1;
 	}
 	
-	/*Parse the provided file one by one*/
+	/*Parse the provided files one by one*/
 	for (i=1; i < argc; i++)
 	{
-		/*Check for provided fileName length*/
+		/*Check fileName length*/
 		if (strlen(argv[i]) > MAX_FILENAME_LEN)
 		{
 			printf("ERROR: Maximum allowed filename length is %d.\n", (MAX_FILENAME_LEN));
 			return 1;
 		}
 		
-		printf("------------ Parsing: \"%s%s\" ------------\n\n", argv[i], INPUT_FILE_EXTENSION);
+		printf("------------ Parsing File: \"%s%s\" ------------\n\n", argv[i], INPUT_FILE_EXTENSION);
 		parseFile(argv[i]);
-		/*Printing Divider between Files*/
 	}
 
 	return 0;
 }
-
-
-
 
 
